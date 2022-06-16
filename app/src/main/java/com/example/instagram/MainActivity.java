0302c0151cd58619e50af38230fcf75cf6c3b241
andy <tmgthemultigamer@gmail.com>
2022-06-16 16:10:07 -0700

    changed compose activity to fragment

--- app/src/main/java/com/example/instagram/MainActivity.java
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -21,7 +21,11 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.content.FileProvider;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import com.example.instagram.fragments.ComposeFragment;
+import com.example.instagram.fragments.PostsFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -39,37 +43,26 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
-    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 42;
 
+    final FragmentManager fragmentManager = getSupportFragmentManager();
     private ConstraintLayout clMain;
-    private EditText etDescription;
-    private ImageView ivPreview;
-    private Button btnPicture;
-    private Button btnSubmit;
-    private Button btnFeed;
     private BottomNavigationView bottomNavigationView;
     BottomNavigationItemView btnHome;
     BottomNavigationItemView btnCompose;
     BottomNavigationItemView btnProfile;
-    private File photoFile;
-    public String photoFileName = "photo.jpg";
+    Fragment fragment;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         clMain = findViewById(R.id.clMain);
-        etDescription = findViewById(R.id.etDescription);
-        ivPreview = findViewById(R.id.ivPreview);
-        btnPicture = findViewById(R.id.btnPicture);
-        btnSubmit = findViewById(R.id.btnSubmit);
-        btnFeed = findViewById(R.id.btnFeed);
         btnHome = findViewById(R.id.action_home);
         btnCompose = findViewById(R.id.action_compose);
         btnProfile = findViewById(R.id.action_profile);
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        fragment = new ComposeFragment();
 
-        ivPreview.setImageResource(R.drawable.ic_baseline_image_24);
         btnCompose.performClick();
         setColors(getColor(R.color.white),
                 getColor(R.color.black),
@@ -79,39 +72,6 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.mipmap.ic_logo);
 
-        btnPicture.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                launchCamera();
-            }
-        });
-
-        btnSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String description = etDescription.getText().toString();
-                if (description.isEmpty()) {
-                    etDescription.onEditorAction(EditorInfo.IME_ACTION_DONE);
-                    Snackbar.make(clMain, "Please fill out a description", Snackbar.LENGTH_LONG).show();
-                    return;
-                }
-                if (photoFile == null || ivPreview.getDrawable() == null) {
-                    Snackbar.make(clMain, "There is no image", Snackbar.LENGTH_LONG).show();
-                    return;
-                }
-                ParseUser currentUser = ParseUser.getCurrentUser();
-                savePost(v, description, currentUser, photoFile);
-            }
-        });
-
-        btnFeed.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, FeedActivity.class);
-                startActivity(intent);
-            }
-        });
-
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -120,97 +80,35 @@ public class MainActivity extends AppCompatActivity {
                         setColors(getColor(R.color.white),
                                 getColor(R.color.black),
                                 getColor(R.color.white));
-                        return true;
+                        fragment = new ComposeFragment();
+                        break;
                     case R.id.action_home:
+                        // TODO: update fragment
                         setColors(getColor(R.color.black),
                                 getColor(R.color.white),
                                 getColor(R.color.white));
-                        return true;
+                        fragment = new PostsFragment();
+                        break;
                     case R.id.action_profile:
+                        // TODO: update fragment
                         setColors(getColor(R.color.white),
                                 getColor(R.color.white),
                                 getColor(R.color.black));
-                        return true;
-                    default: return true;
+                        fragment = new ComposeFragment();
+                        break;
+                    default: break;
                 }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
             }
         });
-
+        bottomNavigationView.setSelectedItemId(R.id.action_compose);
     }
 
     private void setColors(int home, int compose, int profile) {
         btnHome.setBackgroundColor(home);
         btnCompose.setBackgroundColor(compose);
         btnProfile.setBackgroundColor(profile);
-    }
-
-    private void launchCamera() {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        photoFile = getPhotoFileUri(photoFileName);
-
-        Uri fileProvider = FileProvider.getUriForFile(MainActivity.this, "com.codepath.fileprovider", photoFile);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
-
-        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
-                ivPreview = (ImageView) findViewById(R.id.ivPreview);
-                ivPreview.setImageBitmap(takenImage);
-            } else {
-                Snackbar.make(clMain, "Picture wasn't taken", Snackbar.LENGTH_LONG).show();
-            }
-        }
-    }
-
-    private File getPhotoFileUri(String photoFileName) {
-        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
-        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()) {
-            Log.d(TAG, "failed to create directory");
-        }
-        return new File(mediaStorageDir.getPath() + File.separator + photoFileName);
-    }
-
-    private void savePost(View v, String description, ParseUser currentUser, File photoFile) {
-        Post post = new Post();
-        post.setDescription(description);
-        post.setImage(new ParseFile(photoFile));
-        post.setUser(currentUser);
-        post.saveInBackground(new SaveCallback() {
-            @Override
-            public void done(ParseException e) {
-                if (e != null) {
-                    Snackbar.make(v, "Sorry, there was an error saving your post", Snackbar.LENGTH_LONG);
-                    Log.e(TAG, "save post error", e);
-                }
-                etDescription.setText("");
-                ivPreview.setImageResource(0);
-                Log.i(TAG, "post saved");
-                Snackbar.make(clMain, "Post saved!", Snackbar.LENGTH_LONG).show();
-            }
-        });
-    }
-
-    private void queryPosts() {
-        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
-        query.include(Post.KEY_USER);
-        query.findInBackground(new FindCallback<Post>() {
-            @Override
-            public void done(List<Post> objects, ParseException e) {
-                if (e != null) {
-                    Log.i(TAG, "queryPosts error", e);
-                    return;
-                }
-                for (Post post : objects) {
-                    Log.i(TAG, "Post: " + post.getDescription() + " " + post.getUser().getUsername());
-                }
-            }
-        });
     }
 
     @Override
--- app/src/main/java/com/example/instagram/fragments/ComposeFragment.java
+++ app/src/main/java/com/example/instagram/fragments/ComposeFragment.java
@@ -1,66 +1,181 @@
 package com.example.instagram.fragments;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
+import com.example.instagram.FeedActivity;
+import com.example.instagram.Post;
 import com.example.instagram.R;
+import com.google.android.material.snackbar.Snackbar;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ComposeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.io.File;
+import java.util.List;
+
 public class ComposeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    public static final String TAG = "ComposeFragment";
+    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 42;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private ConstraintLayout clCompose;
+    private EditText etDescription;
+    private ImageView ivPreview;
+    private Button btnPicture;
+    private Button btnSubmit;
+    private Button btnFeed;
+    private File photoFile;
+    public String photoFileName = "photo.jpg";
 
-    public ComposeFragment() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ComposeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ComposeFragment newInstance(String param1, String param2) {
-        ComposeFragment fragment = new ComposeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public ComposeFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_compose, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        clCompose = view.findViewById(R.id.clMain);
+        etDescription = view.findViewById(R.id.etDescription);
+        ivPreview = view.findViewById(R.id.ivPreview);
+        btnPicture = view.findViewById(R.id.btnPicture);
+        btnSubmit = view.findViewById(R.id.btnSubmit);
+        btnFeed = view.findViewById(R.id.btnFeed);
+
+        ivPreview.setImageResource(R.drawable.ic_baseline_image_24);
+
+        btnPicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchCamera();
+            }
+        });
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String description = etDescription.getText().toString();
+                if (description.isEmpty()) {
+                    etDescription.onEditorAction(EditorInfo.IME_ACTION_DONE);
+                    Snackbar.make(clCompose, "Please fill out a description", Snackbar.LENGTH_LONG).show();
+                    return;
+                }
+                if (photoFile == null || ivPreview.getDrawable() == null) {
+                    Snackbar.make(clCompose, "There is no image", Snackbar.LENGTH_LONG).show();
+                    return;
+                }
+                ParseUser currentUser = ParseUser.getCurrentUser();
+                savePost(v, description, currentUser, photoFile);
+            }
+        });
+
+        btnFeed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), FeedActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void launchCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        photoFile = getPhotoFileUri(photoFileName);
+
+        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+    }
+
+    private void savePost(View v, String description, ParseUser currentUser, File photoFile) {
+        Post post = new Post();
+        post.setDescription(description);
+        post.setImage(new ParseFile(photoFile));
+        post.setUser(currentUser);
+        post.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    Snackbar.make(v, "Sorry, there was an error saving your post", Snackbar.LENGTH_LONG);
+                    Log.e(TAG, "save post error", e);
+                }
+                etDescription.setText("");
+                ivPreview.setImageResource(0);
+                Log.i(TAG, "post saved");
+                Snackbar.make(clCompose, "Post saved!", Snackbar.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                ivPreview = (ImageView) clCompose.findViewById(R.id.ivPreview);
+                ivPreview.setImageBitmap(takenImage);
+            } else {
+                Snackbar.make(clCompose, "Picture wasn't taken", Snackbar.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private File getPhotoFileUri(String photoFileName) {
+        File mediaStorageDir = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()) {
+            Log.d(TAG, "failed to create directory");
+        }
+        return new File(mediaStorageDir.getPath() + File.separator + photoFileName);
+    }
+
+    private void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> objects, ParseException e) {
+                if (e != null) {
+                    Log.i(TAG, "queryPosts error", e);
+                    return;
+                }
+                for (Post post : objects) {
+                    Log.i(TAG, "Post: " + post.getDescription() + " " + post.getUser().getUsername());
+                }
+            }
+        });
     }
 }
